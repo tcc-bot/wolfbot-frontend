8aa9f18c6473371e4f5d94a94e8bc97740889897
allyson316 <allyson316@gmail.com>
2018-07-13 20:29:06 -0300

    acerto de codificação

--- src/views/index.js
+++ src/views/index.js
@@ -5,22 +5,12 @@ import Carteira from './Carteira'
 import Graficos from './Graficos'
 import Historico from './Historico'
 import Backtesting from './Backtesting'
-import PasswordRecovery from './PasswordRecovery';
-import EmailSendPasswordRecovery from './EmailSendPasswordRecovery';
-import ChangePassword from './ChangePassword';
-import { Login, Page404, Page500, Register } from './Pages'
+
 import {
   Login,
   Page404,
   Page500,
   Register,
-  Dashboard,
-  Carteira,
-  Graficos,
-  Historico,
-  Estatisticas,
-  Backtesting,
-  Configuracao,
   PasswordRecovery,
   EmailSendPasswordRecovery,
   ChangePassword
