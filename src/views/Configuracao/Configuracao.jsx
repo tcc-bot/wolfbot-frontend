c54c47486556bf738efd6a6fae984d8a0277a802
allyson316 <allyson316@gmail.com>
2018-07-15 16:08:48 -0300

    ajustes tela de config

--- src/views/Configuracao/Configuracao.jsx
+++ src/views/Configuracao/Configuracao.jsx
@@ -1,37 +1,67 @@
 import React, { Component } from 'react';
-import { Row } from 'reactstrap';
+import { Row, Col, Card, CardHeader, CardBody, InputGroup, Label, Input, Form } from 'reactstrap';
+
+import Alerts from '../../containers/Components/Alerts'
 
 class Configuracao extends Component {
     constructor(props) {
         super(props);
-
-        this.toggle = this.toggle.bind(this);
-        this.onRadioBtnClick = this.onRadioBtnClick.bind(this);
-
         this.state = {
-            dropdownOpen: false,
-            radioSelected: 2,
+
         };
     }
 
-    toggle() {
-        this.setState({
-            dropdownOpen: !this.state.dropdownOpen,
-        });
-    }
-
-    onRadioBtnClick(radioSelected) {
-        this.setState({
-            radioSelected: radioSelected,
-        });
-    }
-
     render() {
-
         return (
             <div className="animated fadeIn">
+                <Alerts />
                 <Row>
-
+                    <Col xs="12" lg="12" sm="12">
+                        <Card className="card">
+                            <CardHeader>
+                                <i className="icon-settings"></i> Configuração
+                            </CardHeader>
+                            <CardBody>
+                                <Form>
+                                    <InputGroup className="mb-3">
+                                        <Col lg="2">
+                                            <Label >
+                                                <h6>Exchange</h6>
+                                            </Label>
+                                        </Col>
+                                        <Col>
+                                            <select name="moedas" id="moedas">
+                                                <option value="valor1">Valor 1</option>
+                                                <option value="valor2" selected>Valor 2</option>
+                                                <option value="valor3">Valor 3</option>
+                                            </select>
+                                        </Col>
+                                    </InputGroup>
+                                    <InputGroup className="mb-3">
+                                        <Col lg="2">
+                                            <Label>
+                                                <h6>API Key</h6>
+                                            </Label>
+                                        </Col>
+                                        <Col>
+                                            <Input type="text" className="form-control" />
+                                        </Col>
+                                    </InputGroup>
+                                    <InputGroup className="mb-3">
+                                        <Col lg="2">
+                                            <Label>
+                                                <h6>API Secret</h6>
+                                            </Label>
+                                        </Col>
+                                        <Col>
+                                            <Input type="text" className="form-control" />
+                                        </Col>
+                                    </InputGroup>
+                                    <hr />
+                                </Form>
+                            </CardBody>
+                        </Card>
+                    </Col>
                 </Row>
             </div>
         );
