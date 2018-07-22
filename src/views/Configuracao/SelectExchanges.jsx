e7af997f4967f226f7b94efcff9a761a1d49f79f
allyson316 <allyson316@gmail.com>
2018-07-22 13:03:24 -0300

    ajuste de codigo

--- src/views/Configuracao/SelectExchanges.jsx
+++ src/views/Configuracao/SelectExchanges.jsx
@@ -9,19 +9,29 @@ import { getExchanges } from './ConfiguracaoActions'
 class SelectExchanges extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            selectedOptions: ''
+        }
     }
 
-
+    handleChange = (selectedOptions) => {
+        this.setState({ selectedOptions })
+    }
 
     componentWillMount() {
         this.props.getExchanges()
     }
 
     render() {
-        console.log(this.props.data.data)
+        const { selectedOptions } = this.state
+        const value = selectedOptions.value
+
+        console.log(selectedOptions.value)
         return (
             <div>
                 <Select
+                    value={value}
+                    onChange={this.handleChange}
                     options={this.props.data.data}
                 />
             </div>
