c0482938359b5e94cb9c377a79b548fcf5505471
Vinicius Rocha <vnc.chelsea@gmail.com>
2018-08-10 19:56:41 -0300

    fix(App.js): erro para logar pelo /login na rota

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ class App extends Component {
     return (
       <HashRouter>
         <Switch>
-          <Route exact path="/login" name="Login Page" component={Login} />
+          <Route exact path="/login" name="Login Page" component={AuthLogin} />
           <Route exact path="/register" name="Register Page" component={Register} />
           <Route exact path="/404" name="Page 404" component={Page404} />
           <Route exact path="/500" name="Page 500" component={Page500} />
--- src/views/Dashboard/dashboardReducer.js
+++ src/views/Dashboard/dashboardReducer.js
@@ -1,13 +1,13 @@
 const exchangeKey = 'exchange_bot'
 const INITIAL_STATE = {
-    exchange: JSON.parse(localStorage.getItem(exchangeKey))
+  exchange: JSON.parse(localStorage.getItem(exchangeKey))
 }
 export default (state = INITIAL_STATE, action) => {
-    switch (action.type) {
-        case 'EXCHANGE_FETCHED':
-            localStorage.setItem(exchangeKey, JSON.stringify(action.payload.data))
-            return { ...state, exchange: action.payload.data }
-        default:
-            return state
-    }
+  switch (action.type) {
+    case 'EXCHANGE_FETCHED':
+      localStorage.setItem(exchangeKey, JSON.stringify(action.payload.data))
+      return { ...state, exchange: action.payload.data }
+    default:
+      return state
+  }
 }
