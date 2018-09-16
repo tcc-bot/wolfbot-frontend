3f66a5c24e8f163233842ffe6988c34a13c6fa0a
Vinicius Rocha <vnc.chelsea@gmail.com>
2018-09-16 06:38:48 -0300

    fix: reducers

--- src/reducers.js
+++ src/reducers.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux'
 import { reducer as formReducer } from 'redux-form'
 import { reducer as toastrReducer } from 'react-redux-toastr'
 
-import DashboardReducer from './views/Dashboard/DashboardReducer';
+import DashboardReducer from './views/Dashboard/dashboardReducer';
 import AuthReducer from './views/Pages/Login/authReducer';
 import ConfigSelectReducer from './views/Configuracao/SelectReducer';
 import ConfiguracaoReducer from './views/Configuracao/ConfiguracaoReducer';
