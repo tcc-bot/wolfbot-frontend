81f949bdd08d8a34f9b0cb0dd981cf2dca799c6a
Vinicius Rocha <vnc.chelsea@gmail.com>
2018-09-16 09:14:55 -0300

    feat(EmailSendPasswordRecovery): página que confirmação que o email foi enviado

--- src/views/Pages/EmailSendPasswordRecovery/EmailSendPasswordRecovery.jsx
+++ src/views/Pages/EmailSendPasswordRecovery/EmailSendPasswordRecovery.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Card, CardBody, CardGroup, Col, Container, Row } from 'reactstrap';
+import { Card, Button, CardBody, CardFooter, CardGroup, Col, Container, Row } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 import Alerts from '../../../containers/Components/Alerts'
 
@@ -17,15 +18,31 @@ class EmailSendPasswordRecovery extends Component {
           <Row className="justify-content-center">
             <Col md="8">
               <CardGroup>
-                <Card id="cardLogin" className="card p-4">
+                <Card className="cardAccount card p-4">
                   <CardBody>
-                    <h1>Email de recuperação de senha enviado!</h1>
+                    <img src="dist/img/account/email.png" width="125px" height="125px"
+                      style={{ display: "block", margin: "10px auto" }}></img>
+                    <h1 style={{ textAlign: "center" }}>Email para redefinição de senha enviado!</h1>
+                    <p className="text-white">Verifique sua caixa de entrada, pois enviamos um email para que você possa recuperar sua senha</p>
                   </CardBody>
+                  <CardFooter className="pageCardFooter p-4">
+                    <Row className="justify-content-center">
+                      <p className="pageCardText text-muted">Já recuperou sua senha?</p>
+                      <Link to="/login"><Button color="link" className="px-0">Login</Button></Link>
+                    </Row>
+                  </CardFooter>
+                  <CardFooter className="pageCardFooter p-4">
+                    <Row className="justify-content-center">
+                      <img src="dist/img/template/logo-icon.svg" width="30px" height="30px"
+                        style={{ display: "block", margin: "10px auto" }}></img>
+                    </Row>
+                    <p style={{ textAlign: "center" }} className="text-white">Wolfbot</p>
+                  </CardFooter>
                 </Card>
               </CardGroup>
             </Col>
           </Row>
-        </Container>
+        </Container >
       </div >
     );
   }
