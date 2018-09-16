421e29fe1ba4a9852a4bc47c54b743a3c7785841
Vinicius Rocha <vnc.chelsea@gmail.com>
2018-09-16 14:16:51 -0300

    feat(Perfil): Test ExpansionPanel

--- src/views/Perfil/Perfil.jsx
+++ src/views/Perfil/Perfil.jsx
@@ -6,6 +6,13 @@ import { reduxForm, Field } from 'redux-form'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 
+import PropTypes from 'prop-types';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
 class Perfil extends Component {
   constructor(props) {
     super(props);
@@ -28,6 +35,38 @@ class Perfil extends Component {
                 <i className="icon-user"></i> Perfil
               </CardHeader>
               <CardBody>
+                <div className={PropTypes.object.isRequired.root}>
+                  <ExpansionPanel>
+                    <ExpansionPanelSummary
+                      style={{ backgroundColor: "#192024" }}
+                      expandIcon={<ExpandMoreIcon />}>
+                      <Typography className={PropTypes.object.isRequired.heading}>
+                        <a class="text-white">Dados pessoais</a>
+                      </Typography>
+                    </ExpansionPanelSummary>
+                    <ExpansionPanelDetails className="card-style">
+                      <h4>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                        sit amet blandit leo lobortis eget.
+                    </h4>
+                    </ExpansionPanelDetails>
+                  </ExpansionPanel>
+                  <ExpansionPanel>
+                    <ExpansionPanelSummary
+                      style={{ backgroundColor: "#192024" }}
+                      expandIcon={<ExpandMoreIcon />}>
+                      <Typography className={PropTypes.object.isRequired.heading}>
+                        <a class="text-white">Alterar a senha</a>
+                      </Typography>
+                    </ExpansionPanelSummary>
+                    <ExpansionPanelDetails className="card-style">
+                      <h4>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                        sit amet blandit leo lobortis eget.
+                      </h4>
+                    </ExpansionPanelDetails>
+                  </ExpansionPanel>
+                </div>
               </CardBody>
             </Card>
           </Col>
