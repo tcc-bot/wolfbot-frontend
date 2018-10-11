dfdf06d8659993966caa728685bcf773696ec542
allyson316 <allyson316@gmail.com>
2018-10-11 00:51:49 -0300

    backtest

--- src/views/Backtest/screens/ConfigTeste.jsx
+++ src/views/Backtest/screens/ConfigTeste.jsx
@@ -5,15 +5,17 @@ import {
   Nav,
   NavItem,
   NavLink,
-  Button,
+  Label,
   Card,
   CardHeader,
   CardBody,
-  CardTitle,
-  CardText,
+  InputGroup,
   Row,
   Col
 } from 'reactstrap'
+import Input from '../../../components/ui/Input'
+import Switch from 'react-switch'
+import Select from 'react-select'
 import classnames from 'classnames'
 
 class ConfigTeste extends Component {
@@ -39,32 +41,81 @@ class ConfigTeste extends Component {
       <Col xs='12' lg='12' sm='6'>
         <Card className='card-style card' xs='3' lg='12' sm='6'>
           <CardHeader className='card-header-style'>
-            <i className='fa fa-equalizer' /> Testar Estratégia
+            <i className='icon-equalizer' /> Testar Estratégia
           </CardHeader>
           <CardBody>
-            <Nav tabs >
+            <Nav className="nav-tabs" >
               <NavItem>
                 <NavLink
                   className={classnames({ active: this.state.activeTab === '1' })}
-                  className='nav nav-item'
+                  className='nav-item'
                   onClick={() => { this.toggle('1') }}
-                >
-                                    Estratégia
+                > Estratégia
                 </NavLink>
               </NavItem>
               <NavItem>
                 <NavLink
                   className={classnames({ active: this.state.activeTab === '2' })}
-                  className='nav nav-item'
+                  className='nav-item'
                   onClick={() => { this.toggle('2') }}
-                >
-                                    Resultados
+                > Resultados
                 </NavLink>
               </NavItem>
             </Nav>
             <TabContent activeTab={this.state.activeTab}>
               <TabPane tabId='1'>
-                <Row />
+                <Row>
+                <InputGroup className='mb-3'>
+                    <Col className="col-form-label" lg='1'>
+                        <Label>
+                            <h6>Moeda:</h6>
+                        </Label>
+                    </Col>
+                    <Col lg="10">
+                    <Select
+                        onChange={this.handleChange}
+                        options={this.props.exchanges}
+                        value={this.props.selectedOptions}
+                        />
+                    </Col>
+                </InputGroup>
+                <InputGroup className='mb-3'>
+                    <Col className="col-form-label" lg='1'>
+                        <Label>
+                            <h6>Candle:</h6>
+                        </Label>
+                    </Col>
+                    <Col lg="10">
+                    <Select
+                        onChange={this.handleChange}
+                        options={this.props.exchanges}
+                        value={this.props.selectedOptions}
+                        />
+                    </Col>
+                </InputGroup>
+                <InputGroup className='mb-3'>
+                <Col className="col-form-label" lg='1'>
+                        <Label>
+                            <h6>Sell Indicador:</h6>
+                        </Label>
+                    </Col>
+                <Col lg='3' >
+                  <Switch
+                    //checked={this.props.roboLigado}
+                    //onChange={() => this.props.ligarRobo(this.props.roboLigado)}
+                    handleDiameter={28}
+                    offColor='#f86c6b'
+                    onColor='#4dbd74'
+                    offHandleColor='#fff'
+                    onHandleColor='#fff'
+                    height={40}
+                    width={70}
+                    className='react-switch'
+                    id='small-radius-switch'
+                  />
+              </Col>
+              </InputGroup>
+                </Row>
               </TabPane>
               <TabPane tabId='2'>
                 <Row />
