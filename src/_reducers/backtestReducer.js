e4ab05fc7f7074be18da78a8ec8bfac4114526a3
allyson316 <allyson316@gmail.com>
2018-10-12 19:29:47 -0300

    ajustes tela backtest

--- src/_reducers/backtestReducer.js
+++ src/_reducers/backtestReducer.js
@@ -1,31 +1,30 @@
 const INITIAL_STATE = {
-    currencies: [
-        { 'value': 'BTC/USDT', 'label': 'BTC/USDT' },
-        { 'value': 'ETH/USDT', 'label': 'ETH/USDT' },
-        { 'value': 'BCH/USDT', 'label': 'BCH/USDT' }
-    ],
-    candle: [
-        { 'value': '5m', 'label': '5m' },
-        { 'value': '30m', 'label': '30m' },
-        { 'value': '1h', 'label': '1h' },
-        { 'value': '1d', 'label': '1d' },
-    ],
-    indicators: [
-        { 'value': 'MACD', 'label': 'MACD' },
-        { 'value': 'EMA', 'label': 'EMA' },
-        { 'value': 'STOCH', 'label': 'STOCH' },
-        { 'value': 'CCI', 'label': 'CCI' },
-    ]
+  currencies: [
+    { 'value': 'BTC/USDT', 'label': 'BTC/USDT' },
+    { 'value': 'ETH/USDT', 'label': 'ETH/USDT' },
+    { 'value': 'BCH/USDT', 'label': 'BCH/USDT' }
+  ],
+  candle: [
+    { 'value': '5m', 'label': '5m' },
+    { 'value': '30m', 'label': '30m' },
+    { 'value': '1h', 'label': '1h' },
+    { 'value': '1d', 'label': '1d' }
+  ],
+  indicators: [
+    { 'value': 'MACD', 'label': 'MACD' },
+    { 'value': 'EMA', 'label': 'EMA' },
+    { 'value': 'STOCH', 'label': 'STOCH' },
+    { 'value': 'CCI', 'label': 'CCI' }
+  ]
 }
 
 export default function (state = INITIAL_STATE, action) {
-
-    return state
-    // switch (action.type) {
-    //     case 'MOEDA_SELECTED': {
-    //         return { ...state, currenciesSelected: action.payload }
-    //     }
-    //     default:
-    //         return state
-    // }
+  return state
+  // switch (action.type) {
+  //     case 'MOEDA_SELECTED': {
+  //         return { ...state, currenciesSelected: action.payload }
+  //     }
+  //     default:
+  //         return state
+  // }
 }
--- src/views/Backtest/screens/ConfigTeste.jsx
+++ src/views/Backtest/screens/ConfigTeste.jsx
@@ -12,7 +12,10 @@ import {
   InputGroup,
   Row,
   Col,
-  Collapse
+  Collapse,
+  Popover,
+  PopoverBody,
+  Button
 } from 'reactstrap'
 import Input from '../../../components/ui/Input'
 import Switch from 'react-switch'
@@ -20,25 +23,50 @@ import Select from 'react-select'
 import classnames from 'classnames'
 import { reduxForm, Field } from 'redux-form'
 import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 
 class ConfigTeste extends Component {
   constructor (props) {
     super(props)
 
+    this.handleChangeSwitchSellIndicator = this.handleChangeSwitchSellIndicator.bind(this);
+    this.handleChangeSwitchStopLoss = this.handleChangeSwitchStopLoss.bind(this);
     this.toggle = this.toggle.bind(this)
+    this.togglePopover = this.togglePopover.bind(this)
     this.state = {
       activeTab: '1',
+      checkedSellIndicator: false,
+      checkedStopLoss: false,
+      popoverOpen: false,
       colapseMACD: false,
       colapseEMA: false
+
     }
+  }
+
+  onSubmit (values) {
+    console.log(values)
+    //salvarEstrategia(values)
+  }
+
+  handleChangeSwitchSellIndicator(checkedSellIndicator) {
+    this.setState({ checkedSellIndicator });
+  }
+  handleChangeSwitchStopLoss(checkedStopLoss) {
+    this.setState({ checkedStopLoss });
   }
 
   handleChange = (selectedOptions) => {
     this.setState({ selectedOptions })
     if(selectedOptions.value === 'MACD'){
       this.setState({ collapseMACD: !this.state.collapse })
-    }else if(selectedOptions.value === 'EMA'){
+    }else if(selectedOptions.value !== 'MACD'){
+      this.setState({ collapseMACD: false })
+    }
+    if(selectedOptions.value === 'EMA'){
       this.setState({ collapseEMA: !this.state.collapse })
+    }else if(selectedOptions.value !== 'EMA'){
+      this.setState({ collapseEMA: false })
     }
   }
 
@@ -50,8 +78,42 @@ class ConfigTeste extends Component {
     }
   }
 
+  togglePopover() {
+    this.setState({
+      popoverOpen: !this.state.popoverOpen
+    });
+  }
+
   render () {
+    const { handleSubmit } = this.props
     const { selectedOption } = this.state
+    const custonStyle = {
+      control: styles => ({
+        ...styles,
+        backgroundColor: '#515b65',
+        border: '1px solid #23282c'
+      }),
+      option: (styles, { isDisabled, isFocused, isSelected }) => {
+        return {
+          ...styles,
+          backgroundColor: isDisabled ? '#515b65' : isSelected ? '#000000' : isFocused ? 'rgb(26, 36, 44)' : '#515b65',
+          color: isDisabled ? '#000' : isSelected ? '#e4e7ea' : null,
+          cursor: isDisabled ? 'not-allowed' : 'default',
+        };
+      },
+      input: styles => ({
+        ...styles,
+        color: '#e4e7ea',
+      }),
+      placeholder: styles => ({
+        ...styles,
+        color: '#e4e7ea'
+      }),
+      singleValue: styles => ({
+        ...styles,
+        color: '#e4e7ea'
+      })
+    };
     return (
       <Col xs='12' lg='12' sm='6'>
         <Card className='card-style card' xs='3' lg='12' sm='6'>
@@ -79,11 +141,15 @@ class ConfigTeste extends Component {
             </Nav>
             <TabContent activeTab={this.state.activeTab}>
               <TabPane tabId='1'>
+                <form onSubmit={handleSubmit((v) => this.onSubmit(v))}>
                 <Row>
                   <InputGroup className='mb-3'>
                     <Col className='col-form-label text-right' lg='2'>
                       <Label>
-                        <h6>Moeda:</h6>
+                        <a href="javascript:;" className="a"><i className='icon-question' id="popover" onClick={this.togglePopover} color style={{color: '#20a8d8'}} /></a> Moeda:
+                        <Popover placement="right" isOpen={this.state.popoverOpen} target="popover" togglePopover={this.togglePopover}>
+                          <PopoverBody>Selecionar a moeda que será utilizada no backtest, a moeda base sempre será USDT.</PopoverBody>
+                        </Popover>
                       </Label>
                     </Col>
                     <Col lg='8'>
@@ -91,13 +157,17 @@ class ConfigTeste extends Component {
                         onChange={this.handleChange}
                         options={this.props.backtest.currencies}
                         value={selectedOption}
+                        styles={custonStyle}
                       />
                     </Col>
                   </InputGroup>
                   <InputGroup className='mb-3'>
                     <Col className='col-form-label text-right' lg='2'>
                       <Label>
-                        <h6>Candle:</h6>
+                      <a href="javascript:;" className="a"><i className='icon-question' id="popover" onClick={this.togglePopover} color style={{color: '#20a8d8'}} /></a> Candle:
+                        <Popover placement="right" isOpen={this.state.popoverOpen} target="popover" togglePopover={this.togglePopover}>
+                          <PopoverBody>Selecionar a moeda que será utilizada no backtest, a moeda base sempre será USDT.</PopoverBody>
+                        </Popover>
                       </Label>
                     </Col>
                     <Col lg='8'>
@@ -105,13 +175,17 @@ class ConfigTeste extends Component {
                         onChange={this.handleChange}
                         options={this.props.backtest.candle}
                         value={selectedOption}
+                        styles={custonStyle}
                       />
                     </Col>
                   </InputGroup>
                   <InputGroup className='mb-3'>
                     <Col className='col-form-label text-right' lg='2'>
                       <Label>
-                        <h6>Indicador:</h6>
+                      <a href="javascript:;" className="a"><i className='icon-question' id="popover" onClick={this.togglePopover} color style={{color: '#20a8d8'}} /></a> Indicador:
+                        <Popover placement="right" isOpen={this.state.popoverOpen} target="popover" togglePopover={this.togglePopover}>
+                          <PopoverBody>Selecionar a moeda que será utilizada no backtest, a moeda base sempre será USDT.</PopoverBody>
+                        </Popover>
                       </Label>
                     </Col>
                     <Col lg='8'>
@@ -119,6 +193,7 @@ class ConfigTeste extends Component {
                         onChange={this.handleChange}
                         options={this.props.backtest.indicators}
                         value={selectedOption}
+                        styles={custonStyle}
                       />
                     </Col>
                     <Col className="offset-md-2" lg="8">
@@ -126,33 +201,33 @@ class ConfigTeste extends Component {
                       <Card>
                         <CardBody>
                           <InputGroup>
-                            <Col className='col-form-label' lg='2'>
+                            <Col className='col-form-label text-right' lg='2'>
                               <Label>
                                 <h6>MACD LongPeriod:</h6>
                               </Label>
                             </Col>
                             <Col lg='6'>
-                              <Input type='text' name='macdLongPeriod' className='form-control' />
+                              <Field component={Input} type='text' name='macdLongPeriod' className='form-control' placeholder="26" />
                             </Col>
                           </InputGroup>
                           <InputGroup>
-                            <Col className='col-form-label' lg='2'>
+                            <Col className='col-form-label text-right' lg='2'>
                               <Label>
                                 <h6>MACD FastPeriod:</h6>
                               </Label>
                             </Col>
                             <Col lg='6'>
-                              <Input type='text' name='macdFastPeriod' className='form-control' />
+                              <Input type='text' name='macdFastPeriod' className='form-control' placeholder="12" />
                             </Col>
                           </InputGroup>
                           <InputGroup>
-                            <Col className='col-form-label' lg='2'>
+                            <Col className='col-form-label text-right' lg='2'>
                               <Label>
                                 <h6>MACD SignalPeriod:</h6>
                               </Label>
                             </Col>
                             <Col lg='6'>
-                              <Input type='text' name='macdSignalPeriod' className='form-control' />
+                              <Input type='text' name='macdSignalPeriod' className='form-control' placeholder="9" />
                             </Col>
                           </InputGroup>
                         </CardBody>
@@ -162,33 +237,13 @@ class ConfigTeste extends Component {
                       <Card>
                         <CardBody>
                           <InputGroup>
-                            <Col className='col-form-label' lg='2'>
+                            <Col className='col-form-label text-right' lg='2'>
                               <Label>
-                                <h6>MACD LongPeriod:</h6>
+                                <h6>EMA Period:</h6>
                               </Label>
                             </Col>
                             <Col lg='6'>
-                              <Input type='text' name='macdLongPeriod' className='form-control' />
-                            </Col>
-                          </InputGroup>
-                          <InputGroup>
-                            <Col className='col-form-label' lg='2'>
-                              <Label>
-                                <h6>MACD FastPeriod:</h6>
-                              </Label>
-                            </Col>
-                            <Col lg='6'>
-                              <Input type='text' name='macdFastPeriod' className='form-control' />
-                            </Col>
-                          </InputGroup>
-                          <InputGroup>
-                            <Col className='col-form-label' lg='2'>
-                              <Label>
-                                <h6>MACD SignalPeriod:</h6>
-                              </Label>
-                            </Col>
-                            <Col lg='6'>
-                              <Input type='text' name='macdSignalPeriod' className='form-control' />
+                              <Input type='text' name='macdLongPeriod' className='form-control' placeholder="5" />
                             </Col>
                           </InputGroup>
                         </CardBody>
@@ -199,13 +254,16 @@ class ConfigTeste extends Component {
                   <InputGroup className='mb-3'>
                     <Col className='col-form-label text-right' lg='2'>
                       <Label>
-                        <h6>Sell Indicador:</h6>
+                      <a href="javascript:;" className="a"><i className='icon-question' id="popover" onClick={this.togglePopover} color style={{color: '#20a8d8'}} /></a> Venda pelo Indicador:
+                        <Popover placement="right" isOpen={this.state.popoverOpen} target="popover" togglePopover={this.togglePopover}>
+                          <PopoverBody>Selecionar a moeda que será utilizada no backtest, a moeda base sempre será USDT.</PopoverBody>
+                        </Popover>
                       </Label>
                     </Col>
                     <Col lg='3' >
                       <Switch
-                        // checked={this.props.roboLigado}
-                        // onChange={() => this.props.ligarRobo(this.props.roboLigado)}
+                        checked={this.state.checkedSellIndicator}
+                        onChange={this.handleChangeSwitchSellIndicator}
                         handleDiameter={25}
                         offColor='#f86c6b'
                         onColor='#4dbd74'
@@ -221,7 +279,10 @@ class ConfigTeste extends Component {
                   <InputGroup className='mb-3'>
                     <Col className='col-form-label text-right' lg='2'>
                       <Label>
-                        <h6>Profit:</h6>
+                      <a href="javascript:;" className="a"><i className='icon-question' id="popover" onClick={this.togglePopover} color style={{color: '#20a8d8'}} /></a> Lucro:
+                        <Popover placement="right" isOpen={this.state.popoverOpen} target="popover" togglePopover={this.togglePopover}>
+                          <PopoverBody>Selecionar a moeda que será utilizada no backtest, a moeda base sempre será USDT.</PopoverBody>
+                        </Popover>
                       </Label>
                     </Col>
                     <Col lg='8'>
@@ -231,13 +292,16 @@ class ConfigTeste extends Component {
                   <InputGroup className='mb-3'>
                     <Col className='col-form-label text-right' lg='2'>
                       <Label>
-                        <h6>Stop-loss:</h6>
+                      <a href="javascript:;" className="a"><i className='icon-question' id="popover" onClick={this.togglePopover} color style={{color: '#20a8d8'}} /></a> Stop-Loss:
+                        <Popover placement="right" isOpen={this.state.popoverOpen} target="popover" togglePopover={this.togglePopover}>
+                          <PopoverBody>Selecionar a moeda que será utilizada no backtest, a moeda base sempre será USDT.</PopoverBody>
+                        </Popover>
                       </Label>
                     </Col>
                     <Col lg='3' >
                       <Switch
-                        // checked={this.props.roboLigado}
-                        // onChange={() => this.props.ligarRobo(this.props.roboLigado)}
+                        checked={this.state.checkedStopLoss}
+                        onChange={this.handleChangeSwitchStopLoss}
                         handleDiameter={25}
                         offColor='#f86c6b'
                         onColor='#4dbd74'
@@ -246,21 +310,48 @@ class ConfigTeste extends Component {
                         height={35}
                         width={60}
                         className='react-switch'
-                        id='switch-sell-indicator'
+                        id='switch-stop-loss'
                       />
                     </Col>
+                    <Col className="offset-md-2" lg="8">
+                    <Collapse isOpen={this.state.checkedStopLoss}>
+                      <Card>
+                        <CardBody>
+                          <InputGroup>
+                            <Col className='col-form-label text-right' lg='2'>
+                              <Label>
+                                <h6>Percentual:</h6>
+                              </Label>
+                            </Col>
+                            <Col lg='6'>
+                              <Input type='text' name='percentualStopLoss' className='form-control' placeholder="0.1" />
+                            </Col>
+                          </InputGroup>
+                        </CardBody>
+                      </Card>
+                    </Collapse>
+                  </Col>
                   </InputGroup>
                   <InputGroup className='mb-3'>
                     <Col className='col-form-label text-right' lg='2'>
                       <Label>
-                        <h6>Data Inicio:</h6>
+                      <a href="javascript:;" className="a"><i className='icon-question' id="popover" onClick={this.togglePopover} color style={{color: '#20a8d8'}} /></a> Data Inicio:
+                        <Popover placement="right" isOpen={this.state.popoverOpen} target="popover" togglePopover={this.togglePopover}>
+                          <PopoverBody>Selecionar a moeda que será utilizada no backtest, a moeda base sempre será USDT.</PopoverBody>
+                        </Popover>
                       </Label>
                     </Col>
                     <Col lg='8'>
                       <Input type='date' name='date-teste' className='form-control' />
                     </Col>
                   </InputGroup>
+                  <InputGroup>
+                    <Col>
+                      <Button type='submit' className='btn-outline-success'>Testar</Button>
+                    </Col>
+                  </InputGroup>
                 </Row>
+                </form>
               </TabPane>
               <TabPane tabId='2'>
                 <Row />
@@ -275,8 +366,8 @@ class ConfigTeste extends Component {
 
 
 ConfigTeste = reduxForm({ form: 'formBacktest' })(ConfigTeste)
-//const mapDispatchToProps = dispatch => bindActionCreators({ salvarEstrategia }, dispatch)
+const mapDispatchToProps = dispatch => bindActionCreators({ }, dispatch)
 const mapStateToProps = state => ({
   backtest: state.backtest
 })
-export default connect(mapStateToProps)(ConfigTeste)
+export default connect(mapStateToProps, mapDispatchToProps)(ConfigTeste)
