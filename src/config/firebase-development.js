56c7252c63801c1b563f93edb21b7b99fffd6a35
Vinicius Rocha <vnc.chelsea@gmail.com>
2018-10-20 07:47:39 -0300

    arquivos de config

---
+++ src/config/firebase-development.js
@@ -0,0 +1,6 @@
+export default
+  {
+    apiKey: "AIzaSyBQPD1NVqRT0Ru7a11NMsnGpqPLS3lNs7U",
+    authDomain: "wolfbot-development-firebase.firebaseapp.com",
+    databaseUrl: "https://wolfbot-development-firebase.firebaseio.com"
+  }
