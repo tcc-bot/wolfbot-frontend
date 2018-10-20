8ac503838bb4c9608847bcce4471ea69e2d9d4e2
Vinicius Rocha <vnc.chelsea@gmail.com>
2018-10-20 15:08:49 -0300

    remove config firebase

--- src/config/firebase-development.js
+++
@@ -1,6 +0,0 @@
-export default
-  {
-    apiKey: "AIzaSyBQPD1NVqRT0Ru7a11NMsnGpqPLS3lNs7U",
-    authDomain: "wolfbot-development-firebase.firebaseapp.com",
-    databaseUrl: "https://wolfbot-development-firebase.firebaseio.com"
-  }
