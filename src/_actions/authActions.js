1b306fb13d349bf08a41d333d13dc05af1de736e
Vinicius Rocha <vnc.chelsea@gmail.com>
2018-10-30 21:59:59 -0300

    feat: test

--- src/_actions/authActions.js
+++ src/_actions/authActions.js
@@ -1,10 +1,9 @@
 import { toastr } from 'react-redux-toastr'
 import axios from 'axios'
 import api from '../config/config-localhost'
-import { history } from '../helpers/history'
 
+// Firebase Auth - Realizará o login do usuário
 export function login(values) {
-  console.log(values);
   const url = `${api.ACCOUNT_WOLFBOT_URL}/login`
   return dispatch => {
     axios.post(url, values)
@@ -21,7 +20,7 @@ export function login(values) {
   }
 }
 
-// Realizará o cadastro do usuário na API
+// Firebase Auth - Realizará o cadastro do usuário
 export function signup(values) {
   const url = `${api.ACCOUNT_WOLFBOT_URL}/signup`
 
@@ -40,86 +39,7 @@ export function signup(values) {
   }
 }
 
-export function logout() {
-  return { type: 'TOKEN_VALIDATED', payload: false }
-}
-
-export function validateToken(token) {
-  return dispatch => {
-    if (token) {
-      axios.get(`${api.ACCOUNT_WOLFBOT_URL}/me`, { headers: { token: token } })
-        .then(resp => {
-          dispatch({ type: 'TOKEN_VALIDATED', payload: true })
-        })
-        .catch(e => dispatch({ type: 'TOKEN_VALIDATED', payload: false }))
-    } else {
-      dispatch({ type: 'TOKEN_VALIDATED', payload: false })
-    }
-  }
-}
-
-export function passwordRecovery(email) {
-  return dispatch => {
-    axios.post(`${api.ACCOUNT_WOLFBOT_URL}/passwordrecovery`, email)
-      .then(resp => {
-        dispatch({ type: 'PASSWORD_RECOVERY', payload: resp.data.valid }
-          , toastr.success('Sucesso', 'O Email para redefinição de senha foi enviado!')
-        )
-      })
-      .catch(e => toastr.error('Erro', e.response.data.errors.message))
-  }
-}
-
-export function loadLoginPage() {
-  return { type: 'PAGE_LOGIN_UPDATED', payload: false }
-}
-
-export function loadSession() {
-  const USER_BOT = loadLocalStorage('user_bot')
-  return { type: 'LOAD_SESSSION_USER', payload: USER_BOT }
-}
-
-export function loadChangePasswordPage(parameter) {
-  const objChangePassword = {
-    changepasswordhash: parameter
-  }
-  return dispatch => {
-    axios.post(`${api.ACCOUNT_WOLFBOT_URL}/changepasswordpermition`, objChangePassword)
-      .then(resp => {
-        const obj = {
-          success: resp.data.success,
-          hash: resp.data.hash
-        }
-        dispatch({ type: 'CHANGE_PASSWORD_CONFIRM', payload: obj }
-          , toastr.success('Sucesso', 'Realize a alteração da senha'))
-      })
-      .catch(e => {
-        dispatch({ type: 'CHANGE_PASSWORD_DENIED', payload: e.response.data })
-      })
-  }
-}
-
-export function changePassword(values, changePasswordHash) {
-  const objChangePassword = {
-    password: values.password,
-    passwordConfirm: values.passwordConfirm,
-    changePasswordHash: changePasswordHash
-  }
-  return dispatch => {
-    axios.post(`${api.ACCOUNT_WOLFBOT_URL}/changepassword`, objChangePassword)
-      .then(resp => {
-        dispatch({ type: 'PASSWORD_CHANGED', payload: resp.data.success }
-          , toastr.success('Sucesso', resp.data.message))
-      })
-      .catch(e => {
-        for (var i = 0; i < e.response.data.errors.length; i++) {
-          toastr.error('Erro', e.response.data.errors[i].message)
-        }
-      })
-  }
-}
-
-// Action que é chamada quando o usuário clica no link para ativar a conta
+// Firebase Auth - Action que é chamada quando o usuário clica no link para ativar a conta
 export function verifiyActiveAccount(code) {
   return dispatch => {
     axios.get(`${api.ACCOUNT_WOLFBOT_URL}/active`, { headers: { code: code } })
@@ -135,6 +55,92 @@ export function verifiyActiveAccount(code) {
       })
   }
 }
+
+// Firebase Auth - Desloga o usuário
+export function logout() {
+  return { type: 'TOKEN_VALIDATED', payload: false }
+}
+
+// Firebase Auth - Verifica se o token do usuário é valido, se não é então desloga o usuário
+export function validateToken(token) {
+  return dispatch => {
+    if (token) {
+      axios.get(`${api.ACCOUNT_WOLFBOT_URL}/me`, { headers: { token: token } })
+        .then(resp => {
+          dispatch({ type: 'TOKEN_VALIDATED', payload: true })
+        })
+        .catch(e => dispatch({ type: 'TOKEN_VALIDATED', payload: false }))
+    } else {
+      dispatch({ type: 'TOKEN_VALIDATED', payload: false })
+    }
+  }
+}
+
+// Firebase Auth - É chamado quando a página de login é carregada
+export function loadLoginPage() {
+  return { type: 'PAGE_LOGIN_UPDATED', payload: false }
+}
+
+// Firebase Auth - Recupera os dados da sessão do usuário no localStorage
+export function loadSession() {
+  const USER_BOT = loadLocalStorage('user_bot')
+  return { type: 'LOAD_SESSSION_USER', payload: USER_BOT }
+}
+
+export function passwordRecovery(email) {
+  alert('Não implementado');
+  // return dispatch => {
+  //   axios.post(`${api.ACCOUNT_WOLFBOT_URL}/passwordrecovery`, email)
+  //     .then(resp => {
+  //       dispatch({ type: 'PASSWORD_RECOVERY', payload: resp.data.valid }
+  //         , toastr.success('Sucesso', 'O Email para redefinição de senha foi enviado!')
+  //       )
+  //     })
+  //     .catch(e => toastr.error('Erro', e.response.data.errors.message))
+  // }
+}
+export function loadChangePasswordPage(parameter) {
+  alert('Não implementado')
+  // const objChangePassword = {
+  //   changepasswordhash: parameter
+  // }
+  // return dispatch => {
+  //   axios.post(`${api.ACCOUNT_WOLFBOT_URL}/changepasswordpermition`, objChangePassword)
+  //     .then(resp => {
+  //       const obj = {
+  //         success: resp.data.success,
+  //         hash: resp.data.hash
+  //       }
+  //       dispatch({ type: 'CHANGE_PASSWORD_CONFIRM', payload: obj }
+  //         , toastr.success('Sucesso', 'Realize a alteração da senha'))
+  //     })
+  //     .catch(e => {
+  //       dispatch({ type: 'CHANGE_PASSWORD_DENIED', payload: e.response.data })
+  //     })
+  // }
+}
+
+export function changePassword(values, changePasswordHash) {
+  alert('Não implementado');
+  // const objChangePassword = {
+  //   password: values.password,
+  //   passwordConfirm: values.passwordConfirm,
+  //   changePasswordHash: changePasswordHash
+  // }
+  // return dispatch => {
+  //   axios.post(`${api.ACCOUNT_WOLFBOT_URL}/changepassword`, objChangePassword)
+  //     .then(resp => {
+  //       dispatch({ type: 'PASSWORD_CHANGED', payload: resp.data.success }
+  //         , toastr.success('Sucesso', resp.data.message))
+  //     })
+  //     .catch(e => {
+  //       for (var i = 0; i < e.response.data.errors.length; i++) {
+  //         toastr.error('Erro', e.response.data.errors[i].message)
+  //       }
+  //     })
+  // }
+}
+
 
 function loadLocalStorage(key) {
   try {
