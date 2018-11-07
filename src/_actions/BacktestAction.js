52746b8ae07c3dd424a74437a5c62bec52a5c52b
Vinicius Rocha <vnc.chelsea@gmail.com>
2018-11-07 00:33:30 -0200

    ajuste

--- src/_actions/BacktestAction.js
+++ src/_actions/BacktestAction.js
@@ -3,6 +3,8 @@ import { toastr } from 'react-redux-toastr'
 import api from '../config/config-localhost'
 import functions from '../helpers/functions'
 
+const USER_BOT = functions.loadLocalStorage('user_bot')
+
 export function testarStrategy(values) {
   const url = `${api.WOLFBOT_API_URL}/backtest/testarConfiguracao`
   return dispatch => {
